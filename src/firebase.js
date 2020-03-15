203dbfefad37898289c1cc77f303f5c8fdaf20a3
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-03-14 23:42:57 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9b2ljxk-omeIWR3bDOFvkRsLuYnxzoNk",
-    authDomain: "minesweeper-7c394.firebaseapp.com",
-    databaseURL: "https://minesweeper-7c394.firebaseio.com",
-    projectId: "minesweeper-7c394",
-    storageBucket: "minesweeper-7c394.appspot.com",
-    messagingSenderId: "168745128025",
-    appId: "1:168745128025:web:91b428db1db811585be148"
+    //hidden stuff coming soon
 };
 
 // Initialize Firebase
